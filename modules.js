e8c272cd132a32e77decae557f6c8ce5c89bf977
Kostas Chikimtzis <kostaschikis@gmail.com>
2020-07-11 18:49:20 +0300

    removed unnecessary files

--- modules.js
+++
@@ -1,7 +0,0 @@
-const { people, ages } = require('./people.js');
-
-console.log(people, ages);
-
-// Build-In
-const os = require('os')
-console.log(os.platform(), os.homedir())
--- people.js
+++
@@ -1,7 +0,0 @@
-const people = ['yoshi', 'ryu', 'mario']
-const ages = [20, 25, 30]
-
-module.exports = {
- people,
- ages 
-}
